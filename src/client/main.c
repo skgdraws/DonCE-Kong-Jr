b6923baf2ad3ca6f7a9e18a27f597fc3335b3e76
Franco Sagot - SKG Art <60370730+skgdraws@users.noreply.github.com>
2025-11-12 15:46:29 -0600

    SDL2 Client Setup

---
+++ src/client/main.c
@@ -0,0 +1,11 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+/*
+    Project: DonCE-Kong-Jr
+    Client for the game to connect to the java server.
+    Language Standard: C17
+
+    Franco Sagot Lopez - 2022211182
+*/
+
